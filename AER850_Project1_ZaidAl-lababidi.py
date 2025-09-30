154d1e40d6cf34a36ce1c1faf941189c837b324e
ZaidA <z1al@torontomu.ca>
2025-09-30 17:19:42 -0400

    Create AER850_Project1_ZaidAl-lababidi.py

---
+++ AER850_Project1_ZaidAl-lababidi.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+AER 850 Project 1
+By: Zaid Al-lababidi
+501176747
+"""
+
+"""
+Importing Libraries
+"""
+import pandas as pd
+import matplotlib.pyplot as mplot
+import numpy as np
+import seaborn as sb
+import joblib as jl
+
+"""
+Step 1: Import Library
+"""
+data = pd.read_csv("C:\Github\AER850_Codes\Project 1 Data.csv")
+
+print(data.head())
+
+"""
+Step 2: Perform Statistical Analysis
+"""
+fig, ax = mplot.subplots(subplot_kw={"projection":"3d"})
+ax.plot(data["X"], data["Y"], data["Z"])
+mplot.title("Statistical Representation of Data in 3d Space")
+
+fig, ay = mplot.subplots()
+ay.plot(data["Step"], data["X"])
+ay.plot(data["Step"], data["Y"])
+ay.plot(data["Step"], data["Z"])
+
+fig, az = mplot.subplots()
+az.plot(data["X"])
+az.plot(data["Y"])
+az.plot(data["Z"])
+az.plot(data["Step"])
+"""
+Step 3: Correlation Analysis
+"""
+
+x_mean = np.mean(data["X"])
+y_mean = np.mean(data["Y"])
+z_mean = np.mean(data["Z"])
+
+xy_arr = []
+xz_arr = []
+yz_arr = []
+x_arr = []
+y_arr = []
+z_arr = []
+
+for i in range(1, 860):
+    xy_arr.append((data["X"][i] - x_mean)*(data["Y"][i] - y_mean))
+    xz_arr.append((data["X"][i] - x_mean)*(data["Z"][i] - z_mean))
+    yz_arr.append((data["Y"][i] - y_mean)*(data["Z"][i] - z_mean))
+    
+    x_arr.append((data["X"][i] - x_mean)**2)
+    y_arr.append((data["Y"][i] - y_mean)**2)
+    z_arr.append((data["Z"][i] - z_mean)**2)
+    
+r_xy = sum(xy_arr)/np.sqrt(sum(x_arr)*sum(y_arr))
+r_xz = sum(xz_arr)/np.sqrt(sum(x_arr)*sum(z_arr))
+r_yz = sum(yz_arr)/np.sqrt(sum(y_arr)*sum(z_arr))
